36a0eb22500b1d5e2fd8d4b796b6b938c0e715eb
Henrik Nielsen <henrik.helfred@gmail.com>
2020-12-18 16:02:33 +0100

    Loop indført

--- test2.py
+++ test2.py
@@ -1,3 +1,7 @@
 # Dette er et lille test program som udskriver det Hej + input-navn
 name_input = input("Enter Name: ")
-print("Hej " + name_input)
+
+i = 1
+while i <= 10:
+    print("Hej " + name_input)
+    i = i + 1
