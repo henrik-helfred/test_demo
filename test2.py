1a907230f96215261771640ef9df7f908555aee3
Henrik Nielsen <75535516+henrik-helfred@users.noreply.github.com>
2020-12-18 17:02:04 +0100

    Update test2.py
    Start I sat til 0.

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 # Dette er et lille test program som udskriver det Hej + input-navn
 name_input = input("Enter Name: ")
 
-i = 1
+i = 0
 while i <= 10:
     print("Hej " + name_input)
     i = i + 1
