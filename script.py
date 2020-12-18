d64a386c0ffa1d1047555807d0bc0a902b2e5e26
Henrik Nielsen <henrik.helfred@gmail.com>
2020-12-18 15:51:10 +0100

    test

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ import requests
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+
+# Her er en kommentar
---
+++ test.py
@@ -0,0 +1,8 @@
+def hest(navn):
+    i = 0
+    while i <= 5:
+        print("Hej " + navn)
+        i = i + 1
+
+
+hest("Henrik")
