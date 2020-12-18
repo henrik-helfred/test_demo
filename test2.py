9c4ebe8b937194e6ef429b7a19a7fe391cd276b7
Henrik Nielsen <henrik.helfred@gmail.com>
2020-12-18 17:12:09 +0100

    Tilføjet efternavn

--- test2.py
+++ test2.py
@@ -1,7 +1,8 @@
 # Dette er et lille test program som udskriver det Hej + input-navn
-name_input = input("Enter Name: ")
+name_input = input("Enter FirstName: ")
+lastname_input = input("Enter LastName: ")
 
 i = 0
-while i <= 10:
-    print("Hej " + name_input)
+while i < 10:
+    print("Hej " + name_input + " " + lastname_input)
     i = i + 1
